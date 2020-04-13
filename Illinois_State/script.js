09b2d4a9dc1f795a1dcec45ea6176902bc26f8f8
TJReynolds88 <tj.reynolds1988@gmail.com>
2020-04-13 17:12:34 -0500

    Add files via upload

--- Illinois_State/script.js
+++ Illinois_State/script.js
@@ -1,6 +1,11 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+
 
 
 function setDetails(imageUrl, titleText) {
@@ -36,9 +41,45 @@ function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     })
 }
 
 function getThumbnailsArray() {
-    'use strict'
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailsArray = [].slice.call(thumbnails);
+    return thumbnailsArray;
 }
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    frame.classList.remove(TINY_EFFECT_CLASS);
+}
+
+function addKeyPressHandler() {
+    'use strict';
+    document.body.addEventListener('keyup', function(event){
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
+}
+
+initializeEvents();
